634c6cec6c5ff9d3910cca70fbb94a3a30c0750e
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-10-01 00:01:51 +0100

    update syntax

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 
-export default function returnHowManyArguments(...args) {
-  return args.length;
+export default function concatArrays(array1, array2, string) {
+  return [...array1, ...array2, ...string.split('')];
 }
