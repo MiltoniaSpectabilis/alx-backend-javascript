91287f2cfe5acb4b0648c67e706de9447365f4ed
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-10-01 00:27:09 +0100

    For...of Loops

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+export default function appendToEachArrayValue(array, appendString) {
+  for (let value of array) {
+    let idx = array.indexOf(value);
+    array[idx] = appendString + value;
+  }
+
+  return array;
+}
