d920c6e938d8fcdc61ff9d381745bb9f6ce7cf6b
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-10-01 00:43:23 +0100

    Let's create a report object

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+export default function createReportObject(employeesList) {
+  const allEmployees = { ...employeesList };
+
+  return {
+    allEmployees,
+    getNumberOfDepartments() {
+      return Object.keys(allEmployees).length;
+    },
+  };
+}
