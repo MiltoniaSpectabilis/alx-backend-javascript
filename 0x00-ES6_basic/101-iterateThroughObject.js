df797e2a5fce591ea8fc2cd671516c0f6689b362
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-10-01 01:21:42 +0100

    Iterate through object

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,12 @@
+#!/bin/bash/node
+
+export default function iterateThroughObject(reportWithIterator) {
+  let result = '';
+  for (const employee of reportWithIterator) {
+    if (result) {
+      result += ' | ';
+    }
+    result += employee;
+  }
+  return result;
+}
