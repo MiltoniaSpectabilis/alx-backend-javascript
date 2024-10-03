d34d913c889e90c6e540840c154f9755e9099abd
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-10-04 00:30:48 +0100

    push all

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  cloneCar() {
+    const Clone = this.constructor[Symbol.species] || this.constructor;
+    return new Clone();
+  }
+}
---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,18 @@
+#!/bin/bash/node
+
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  cloneCar() {
+    return new Car(this._brand, this._motor, this._color);
+  }
+}
