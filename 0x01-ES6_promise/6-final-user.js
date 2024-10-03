278e27756ea1e7f09386f9c69c0f3fe97d8e0d38
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-10-04 00:12:02 +0100

    eslint

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -7,10 +7,8 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((results) =>
-    results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason.toString(),
-    }))
-  );
+  ]).then((results) => results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : result.reason.toString(),
+  })));
 }
