1127a98107eb81b80c4fbd8bd3ccc4ca155e20f6
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-10-04 00:38:52 +0100

    another attempt

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+  const sizes = [19, 20, 34];
+  return sizes.map((size) => new ClassRoom(size));
 }
