68a74a9f25f48c25e5c8758ea907a80da6a7b66f
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-10-09 02:52:01 +0100

    another attempt

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,13 @@
-// eslint-disable-next-line no-unused-vars
 function cleanSet(set, startString) {
-  if (!(set instanceof Set) || typeof startString !== 'string' || startString === '') {
+  if (typeof startString !== 'string' || startString === '') {
     return '';
   }
 
-  const filteredValues = Array.from(set)
-    .filter((value) => typeof value === 'string' && value.startsWith(startString))
-    .map((value) => value.slice(startString.length));
+  const filteredArray = Array.from(set)
+    .filter(value => typeof value === 'string' && value.startsWith(startString))
+    .map(value => value.slice(startString.length));
 
-  return filteredValues.join('-');
+  return filteredArray.join('-');
 }
+
+export default cleanSet;
